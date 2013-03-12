2d852e1bbbd274c6132348c21c38e7f35fd52d8b
Peter M. Elias <petermelias@gmail.com>
2013-03-11 19:10:18 -0700

    stupid mistake fixed

--- flask_boilerplate/views/helpers.py
+++ flask_boilerplate/views/helpers.py
@@ -3,8 +3,8 @@ from flask import redirect, request, url_for
 def redirect_to_home(): pass # requires config to implement
 
 def redirect_self(clear_args=False):
-	if clear_args:
-		return redirect(url_for(request.endpoint))
+    if clear_args:
+        return redirect(url_for(request.endpoint))
     return redirect(url_for(request.endpoint, **request.view_args))
 
 redirect_to_self = redirect_self
